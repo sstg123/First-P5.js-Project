ec7e292f3afc92d6a77fb728b3d5d319bd9fafbb
sstg123 <71703766+sstg123@users.noreply.github.com>
2020-10-25 09:58:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload()  {
+
+
+
+}
+
+
+function setup()  {
+
+canvas = createCanvas(640, 480);
+canvas.position(150, 150);
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw()  {
+
+image(video, 0, 0, 640, 480);
+
+}
+
+function take_photo()  {
+
+save('student_name.png');
+
+}
+
+
